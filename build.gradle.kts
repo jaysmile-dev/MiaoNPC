2d5986be367c55b751e7eb3a87631aa0e9e68024
jaysmile-dev <jayjaysmile@outlook.de>
2022-04-16 22:06:50 +0200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,10 @@ plugins {
 }
 
 group = "io.papermc.paperweight"
-version = "1.0.0-SNAPSHOT"
-description = "Test plugin for paperweight-userdev"
+version = "1.0.0"
+description = "NPC Plugin"
 
 java {
-  // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
   toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 repositories {
@@ -22,31 +21,24 @@ repositories {
 dependencies {
 
   paperDevBundle("1.18.2-R0.1-SNAPSHOT")
-  // paperweightDevBundle("com.example.paperfork", "1.18.2-R0.1-SNAPSHOT")
 
-  // You will need to manually specify the full dependency if using the groovy gradle dsl
-  // (paperDevBundle and paperweightDevBundle functions do not work in groovy)
-  // paperweightDevelopmentBundle("io.papermc.paper:dev-bundle:1.18.2-R0.1-SNAPSHOT")
 }
 
 tasks {
-  // Configure reobfJar to run when invoking the build task
   assemble {
     dependsOn(reobfJar)
   }
 
   compileJava {
-    options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    options.encoding = Charsets.UTF_8.name()
 
-    // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
-    // See https://openjdk.java.net/jeps/247 for more information.
     options.release.set(17)
   }
   javadoc {
-    options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    options.encoding = Charsets.UTF_8.name()
   }
   processResources {
-    filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+    filteringCharset = Charsets.UTF_8.name()
 
     from(sourceSets.main.get().resources.srcDirs) {
       filesMatching("plugin.yml") {
@@ -60,16 +52,8 @@ tasks {
 
   }
 
-  /*
-  reobfJar {
-    // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
-    // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
-    outputJar.set(layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar"))
-  }
-   */
 }
 
-// Configure plugin.yml generation
 bukkit {
   load = BukkitPluginDescription.PluginLoadOrder.STARTUP
   main = "de.miao.miaonpc.MiaoMain"
