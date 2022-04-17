079edfc377059f3df3f7207ea74d171d4a684078
jaysmile-dev <jayjaysmile@outlook.de>
2022-04-17 16:37:14 +0200

    update

--- src/main/java/de/miao/miaonpc/MiaoMain.java
+++ src/main/java/de/miao/miaonpc/MiaoMain.java
@@ -11,7 +11,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MiaoMain extends JavaPlugin implements Listener {
+public final class MiaoMain extends JavaPlugin {
   private static String prefix = "§9Miao §7-> ";
 
   @Override
